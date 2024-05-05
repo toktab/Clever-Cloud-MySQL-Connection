bea0fb710c5295c9581a66223fac767182532fdc
toktab <tabagaritoko834@gmail.com>
2024-05-05 13:23:43 -0700

    Added a repository-> UserRepository

---
+++ src/main/java/dev/toktab/clevercloud/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package dev.toktab.clevercloud.repository;
+
+import dev.toktab.clevercloud.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, String> {
+}
