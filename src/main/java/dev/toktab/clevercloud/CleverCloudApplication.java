71bc90f573879026fadf46726affed9ce3462980
toktab <tabagaritoko834@gmail.com>
2024-05-05 13:02:00 -0700

    Initialization - Using java 17

---
+++ src/main/java/dev/toktab/clevercloud/CleverCloudApplication.java
@@ -0,0 +1,13 @@
+package dev.toktab.clevercloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CleverCloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CleverCloudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/toktab/clevercloud/CleverCloudApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.toktab.clevercloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CleverCloudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
